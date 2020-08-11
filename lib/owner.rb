f1db33229641c60e224c029921ac54beb1115d65
Alan Coomes <acoomes1@cord.edu>
2020-08-11 23:08:22 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,43 @@
+require 'pry'
 class Owner
-  # code goes here
+  
+  attr_accessor   
+  attr_reader :name, :species
+
+    @@all = []
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+  end
+
+  def name
+     @name.freeze
+   end
+   
+   def species
+     @species.freeze
+   end
+   
+   def say_species
+      "I am a human."
+   end
+   
+   def self.all 
+     @@all 
+   end
+   
+   def self.count
+     @@all.count
+   end 
+   
+   def self.reset_all
+     @@all.clear
+   end 
+   
+   def cats
+     Cat.all.collect {|cat| cat}
+   end
+   
 end
